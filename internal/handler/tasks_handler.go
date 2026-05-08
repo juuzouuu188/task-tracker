da854e72df499b3b5cc2f9f6b32078bcf7cb3eee
Shaune Ong <shauneong@hpe-dxcw4ry4rh.station>
2026-05-09 00:28:20 +0100

    feat: task_handler

--- internal/handler/tasks_handler.go
+++ internal/handler/tasks_handler.go
@@ -1,10 +1,36 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"net/http/httptest"
+	"time"
+
+	"github.com/juuzouuu188/task-tracker/internal/model"
 )
 
-func CreateTaskHandler(rr *httptest.ResponseRecorder, req *http.Request) {
-	
+var tasks = map[string]model.Task{}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title string `json:"title"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&input)
+
+	task := model.Task{
+		ID:     generateID(),
+		Title:  input.Title,
+		Status: model.StatusTodo,
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
+}
+
+func generateID() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
 }

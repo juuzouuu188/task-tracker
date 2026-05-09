16a4e2b74fc99b4e86c43b0dad49502dd23bd06c
Shaune Ong <shauneong@hpe-dxcw4ry4rh.station>
2026-05-09 01:12:22 +0100

    feat: POST /tasks and GET /tasks with tests

--- internal/handler/tasks_handler.go
+++ internal/handler/tasks_handler.go
@@ -12,11 +12,18 @@ import (
 var tasks = map[string]model.Task{}
 
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+
 	var input struct {
 		Title string `json:"title"`
 	}
 
 	json.NewDecoder(r.Body).Decode(&input)
+
+	if input.Title == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	task := model.Task{
 		ID:     generateID(),
@@ -29,6 +36,18 @@ func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(task)
+}
+
+func ListAllTasks(w http.ResponseWriter, r *http.Request) {
+	taskList := []model.Task{}
+
+	for _, task := range tasks {
+		taskList = append(taskList, task)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(taskList)
 }
 
 func generateID() string {
--- internal/handler/tasks_handler_test.go
+++ internal/handler/tasks_handler_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 func TestCreateTask(t *testing.T) {
+	tasks = map[string]model.Task{} // reset
 	body := bytes.NewBufferString(`{"title": "Buy groceries"}`)
 
 	req := httptest.NewRequest(http.MethodPost, "/", body)
@@ -34,4 +35,66 @@ func TestCreateTask(t *testing.T) {
 	if task.Status != model.StatusTodo {
 		t.Errorf("expected status 'todo' got '%s'", task.Status)
 	}
+
+	t.Run("Empty Title", func(t *testing.T) {
+		body := bytes.NewBufferString(`{"title": ""}`)
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set("Content-Type", "application.json")
+
+		rr := httptest.NewRecorder()
+		CreateTaskHandler(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected 400 got %d", rr.Code)
+		}
+	})
+
+}
+
+func TestListAllTasks(t *testing.T) {
+	tasks = map[string]model.Task{} // reset
+	// seed the map with a task
+	tasks["1"] = model.Task{ID: "1", Title: "Buy groceries", Status: model.StatusTodo}
+	tasks["2"] = model.Task{ID: "2", Title: "Buy stationary", Status: model.StatusTodo}
+
+	req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+	req.Header.Set("Content-Type", "application.json")
+
+	rr := httptest.NewRecorder()
+
+	ListAllTasks(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200 got %d", rr.Code)
+	}
+
+	var result []model.Task
+	json.NewDecoder(rr.Body).Decode(&result)
+
+	if len(result) != 2 {
+		t.Errorf("expected 2 tasks got %d", len(result))
+	}
+
+	//Empty List Should still be a valid Response
+	t.Run("Empty ListAllTasks", func(t *testing.T) {
+		tasks = map[string]model.Task{} // reset
+		req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+		req.Header.Set("Content-Type", "application.json")
+
+		rr := httptest.NewRecorder()
+
+		ListAllTasks(rr, req)
+
+		if rr.Code != http.StatusOK {
+			t.Errorf("expected 200 got %d", rr.Code)
+		}
+
+		var result []model.Task
+		json.NewDecoder(rr.Body).Decode(&result)
+
+		if len(result) != 0 {
+			t.Errorf("expected 0 tasks got %d", len(result))
+		}
+
+	})
 }
